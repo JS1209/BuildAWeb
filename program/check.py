94a4f0f5dbf4c19686f51a534ee2b6db06dfc070
JS1209 <J.a.schoenmakers@outlook.com>
2022-10-10 14:30:38 +0200

    now works more generic. Can specify what file to read, what file to append and what word you're looking for

--- program/check.py
+++ program/check.py
@@ -1,20 +1,20 @@
-def search_str(file_path, word):
-  with open(file_path, 'r') as file:
+def search_str(read_file, write_file, word):
+  with open(read_file, 'r') as file:
     lines = file.readlines()
     for line in lines:
       if line.find(word) != -1:
         found = True
-        getFunction(file_path, lines.index(line))
+        getFunction(read_file, write_file, lines.index(line))
         break
 
-def getFunction(file_path, line_number):
+def getFunction(read_file, write_file, line_number):
   x = 0
   while True:
-    with open(file_path, 'r') as file:
+    with open(read_file, 'r') as file:
       for i in range(line_number):
         next(file, None)
       for line in file:
-        writeFunctionToFile(line)
+        writeFunctionToFile(write_file, line)
         if line.find('{') != -1:
           x = x + line.count('{')
         if x == 0:
@@ -25,8 +25,11 @@ def getFunction(file_path, line_number):
           break
       break
 
-def writeFunctionToFile(line):
-  with open('functions.js', 'a') as file:
+def writeFunctionToFile(write_file, line):
+  with open(write_file, 'a') as file:
     file.write(line)
 
-search_str(r'text.txt', 'MakeHouse')
+
+# Syntax is: search(r'<FILENAME_TO_READ_FROM>', '<FILENAME_TO_WRITE_TO>', '<STRING_TO_SEARCH>')
+search_str(r'text.txt', 'text_copied.txt', 'MakeHouse')
+search_str(r'fromHere.js', 'toHere.js', 'updateMySpace')
